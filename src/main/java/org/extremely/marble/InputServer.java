5e0630666178b68bfb495d8ea7ba87b4f254a00d
barak <barak.levinson@symbolab.com>
2023-09-02 15:52:58 +0300

    Convert input to serial instead of TCP

--- src/main/java/org/extremely/marble/InputServer.java
+++ src/main/java/org/extremely/marble/InputServer.java
@@ -1,109 +1,121 @@
 package org.extremely.marble;
 
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.DriverManager;
+import gnu.io.SerialPort;
 import org.joml.Math;
 import org.joml.Vector3f;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.function.Consumer;
 
 import static java.lang.Float.parseFloat;
 
 public class InputServer {
-    public void run(Consumer<Vector3f> consumer) {
-        var serverThread = new Thread(() -> {
-            try (var server = new ServerSocket(9999)) {
-                System.out.println("Input server is running on port " + server.getLocalPort());
 
-                while (true) {
-                    var client = server.accept();
-                    System.out.println("Client connected: " + client.getInetAddress().getHostAddress());
+    private static final int VECTOR_ARRAY_SIZE = 130;
+    private final Vector3f[] vectorArray;
+    private int vectorArrayIndex = 0;
 
-                    spawnClientThread(client, consumer);
-                }
-            } catch (Exception e) {
-                throw new RuntimeException("Cannot listen for game pad", e);
-            }
-        });
-        serverThread.setName("Input Server Thread");
-        serverThread.setDaemon(true);
-        serverThread.start();
-    }
-
-    private void spawnClientThread(Socket clientSocket, Consumer<Vector3f> consumer) {
-        try {
-            var handler = new InputClientHandler(consumer, clientSocket);
-            var thread = new Thread(handler::run);
-            thread.setName("Input Thread");
-            thread.setDaemon(true);
-            thread.start();
-        } catch (Exception e) {
-            throw new RuntimeException("Could not create input client handler", e);
+    public InputServer() {
+        this.vectorArray = new Vector3f[VECTOR_ARRAY_SIZE];
+        for (int i = 0; i < VECTOR_ARRAY_SIZE; i++) {
+            vectorArray[i] = new Vector3f();
         }
     }
 
-    private class InputClientHandler {
-        private static final int VECTOR_ARRAY_SIZE = 130;
-        private final Consumer<Vector3f> consumer;
-        private final Socket clientSocket;
-        private final Scanner reader;
-        private final Vector3f[] vectorArray;
+    public void run(Consumer<Vector3f> consumer) {
+        try {
+            connect("COM6", consumer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        private boolean running = false;
-        private int vectorArrayIndex = 0;
+    void connect(String portName, final Consumer<Vector3f> consumer) throws Exception {
+        System.setProperty("os.name", "Windows 10");
 
-        public InputClientHandler(Consumer<Vector3f> consumer, Socket clientSocket) throws Exception {
-            this.consumer = consumer;
-            this.clientSocket = clientSocket;
-            this.reader = new Scanner(clientSocket.getInputStream());
-            this.vectorArray = new Vector3f[VECTOR_ARRAY_SIZE];
-            for (int i = 0; i < VECTOR_ARRAY_SIZE; i++) {
-                vectorArray[i] = new Vector3f();
+        DriverManager.getInstance().loadDrivers();
+
+        Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier portIdentifier = portEnum.nextElement();
+            System.out.println(portIdentifier.getName());
+        }
+
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if (portIdentifier.isCurrentlyOwned()) {
+            System.out.println("Error: Port is currently in use");
+        } else {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
+
+            if (commPort instanceof SerialPort serialPort) {
+                serialPort.setSerialPortParams(115200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+                InputStream in = serialPort.getInputStream();
+                (new Thread(new SerialReader(in, (text -> process(text, consumer))))).start();
+            } else {
+                System.out.println("Error: Only serial ports are handled by this example.");
             }
+        }
+    }
+
+    private static class SerialReader implements Runnable {
+        private final InputStream in;
+        private final Consumer<String> processor;
+
+        public SerialReader(InputStream in, Consumer<String> processor) {
+            this.in = in;
+            this.processor = processor;
         }
 
         public void run() {
-            running = true;
+            var command = new StringBuilder();
+            int len;
             try {
-                while (running) {
-                    var text = reader.nextLine();
-                    System.out.println("GOT - " + text);
-                    var values = text.split(",");
-
-                    var inVec = new Vector3f(parseFloat(values[0]), parseFloat(values[1]), parseFloat(values[2])).normalize();
-
-                    vectorArray[vectorArrayIndex++] = inVec;
-                    if (vectorArrayIndex == VECTOR_ARRAY_SIZE) {
-                        vectorArrayIndex = 0;
+                while ((len = this.in.read()) > -1) {
+                    if (len == '\n') {
+                        processor.accept(command.toString());
+                        command.setLength(0);
+                    } else {
+                        command.append((char) len);
                     }
-
-                    var reduced = new Vector3f();
-                    for (int i = 0; i < VECTOR_ARRAY_SIZE; i++) {
-                        reduced.add(vectorArray[i]);
-                    }
-                    reduced.div(VECTOR_ARRAY_SIZE);
-                    reduced.normalize();
-
-                    var roll = Math.atan2(reduced.y, reduced.z) * 57.3f;
-                    var pitch = Math.atan2((-reduced.x) , Math.sqrt(reduced.y * reduced.y + reduced.z * reduced.z)) * 57.3f;
-                    var yaw = 0f;
-
-                    consumer.accept(new Vector3f(roll, pitch, yaw));
                 }
-            } catch (Exception e) {
-                shutdown();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
+    }
 
-        private void shutdown(){
-            running = false;
-            try {
-                clientSocket.close();
-            } catch (Exception e) {
-                System.out.println("Cannot gracefully close client socket");
+    private void process(String text, Consumer<Vector3f> consumer) {
+//        System.out.println(text);
+//
+        var values = text.split(",");
+        try {
+            var inVec = new Vector3f(parseFloat(values[0]), parseFloat(values[1]), parseFloat(values[2])).normalize();
+
+            vectorArray[vectorArrayIndex++] = inVec;
+            if (vectorArrayIndex == VECTOR_ARRAY_SIZE) {
+                vectorArrayIndex = 0;
             }
-            System.out.println("Client closed connection, ip=" + clientSocket.getInetAddress().getHostAddress());
+
+            var reduced = new Vector3f();
+            for (int i = 0; i < VECTOR_ARRAY_SIZE; i++) {
+                reduced.add(vectorArray[i]);
+            }
+            reduced.div(VECTOR_ARRAY_SIZE);
+            reduced.normalize();
+
+            var roll = Math.atan2(reduced.y, reduced.z) * 57.3f;
+            var pitch = Math.atan2((-reduced.x), Math.sqrt(reduced.y * reduced.y + reduced.z * reduced.z)) * 57.3f;
+            var yaw = 0f;
+
+            consumer.accept(new Vector3f(roll, pitch, yaw));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
